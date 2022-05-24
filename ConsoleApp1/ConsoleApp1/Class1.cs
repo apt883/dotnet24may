2e93ac2816673c6588f632697e296946ea18dc98
avinash <avinashtangade2312@gmail.com>
2022-05-24 17:54:23 +0530

    first commit

---
+++ ConsoleApp1/ConsoleApp1/Class1.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp1
+{
+    class Class1
+    {
+
+        //int n, m;
+        //    int n = Convert.ToInt32(Console.ReadLine());
+        //    int m = Convert.ToInt32(Console.ReadLine());
+        //    for (int i = 1; i <= n; i++)
+        //    {
+        //        for (int j = 1; j <= m; j++)
+        //        {
+        //            if (i == 1 || j == 1 || i == n || i == j || j == n - i + 1)
+        //            {
+        //                Console.Write("*");
+        //            }
+        //            else
+        //            {
+        //                Console.Write(" ");
+        //            }
+        //        }
+        //    }
+        //    Console.WriteLine();
+
+        // int n, r, sum = 0, temp;
+        // Console.
+        // n=int.parse(Console.ReadLine());
+        // temp=nameof;
+        // while(n > 0)
+        //{  
+        // r = n %10;
+        // sum=(sum*10)+r;
+        // n=n/10;
+
+        //}
+
+        //   if(temp=sum)
+        //   Console.Write("number is pallindrome.");
+        //   else
+        //  Console.Write("number is not pallindrome ");
+
+
+
+                   // int[] myArray = new int[] { 0, 1, 2, 3, 13, 9, 6 };
+                   // int largest = int.MinValue;
+                   // int second = int.MinValue;
+                   //foreach(int i in myArray)
+                   //{
+                   //     if(i > largest )
+                   //  {
+                   //      second=largest;
+                   //       largest= i;
+                   //   }
+                   //     else if(i> second)
+                   //     second =int;
+                   // }
+                   //   System.Console.WriteLine(second);
+                    
+    }
+}
---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,210 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("Hello World!");
+            //Console.ReadLine();
+            //typecast
+            //int a = (int)3.5;
+            //Console.WriteLine(a);
+            //Console.WriteLine("Hello World!");
+            //Console.ReadLine();
+            //typecast
+            //int a = (int)3.5;
+            //Console.WriteLine(a);
+
+            //string k = Convert.ToString(50);
+            // Console.WriteLine(k);
+            //concatenate
+            //Console.WriteLine("Enter Your Name");
+            //string name = Console.ReadLine();
+            //Console.WriteLine("Hey Hello "  + name);
+            //int sum = 0; int a, int b;
+            //Console.WriteLine("Enter Two Numbers");
+            //string name = Console.ReadLine("a");
+            //string name = Console.ReadLine("b");
+            //sum = (a + b);
+            //Console.WriteLine("sum");
+            //string name = Console.ReadLine();
+            //Console.WriteLine("Enter Your Name");
+            //string name =Console.ReadLine();
+            //Console.WriteLine("Hey Hello " + name);
+            //Console.WriteLine("how many pens u want ?");
+            //string pen = Console.ReadLine();
+
+            //odd even program
+
+            // Console.WriteLine("enter a number " );
+            // int n = 20;
+            // string num=Console.ReadLine();
+            //if((
+            // if(n%2==0)
+            // Console.WriteLine(" number is even ");
+            // else
+            // Console.WriteLine(" number is odd ");
+            //Console.ReadLine();
+
+
+            //voting age program
+            //int  age = 18;
+            //if(  age>=18)
+            //Console.WriteLine("eligible to vote");
+            //else if( age>60)
+            //Console.WriteLine("senior citizen ");
+            //else
+            //Console.WriteLine(" Not eligible to vote");
+
+
+            //switch case
+            //  Console.WriteLine("Enter your choice  ");
+            //  string s = Console.ReadLine();
+            //  Console.WriteLine("value Entered from user is :  "  + Convert.ToInt32(s));
+            //int choice = Convert.ToInt32(s);
+            //  switch(choice)
+            //  {  case 1:
+            //    Console.WriteLine("monday");
+            //  break;
+            //  case 2:
+            //  Console.WriteLine("tuesday");
+            //  break;
+            //  case 3:
+            //  Console.WriteLine("wednesday");
+            //  break;
+            //  case 4:
+            //  Console.WriteLine("thursday");
+            //  break;
+            //  case 5:
+            //  Console.WriteLine("friday");
+            //  break;
+            //  case 6:
+            //  Console.WriteLine("saturday");
+            //  break;
+            //  case 7:
+            //  Console.WriteLine("sunday");
+            //  break;
+            //  default:
+            //       Console.WriteLine("hi default case");
+            //  break;  }
+            //Console.ReadLine()
+
+
+            //calculator
+
+
+
+            //Console.WriteLine("Enter your choice  ");
+            //string s = Console.ReadLine();
+            //Console.WriteLine("value Entered from user is :  " + Convert.ToInt32(s));
+            //int choice = Convert.ToInt32(s);
+            //switch (choice)
+            //{
+            //    case 1:
+            //        Console.WriteLine("addition");
+            //        break;
+            //    case 2:
+            //        Console.WriteLine("subtraction");
+            //        break;
+            //    case 3:
+            //        Console.WriteLine("multiplication");
+            //        break;
+            //    case 4:
+            //        Console.WriteLine("divide");
+            //        break;
+            //    default:
+            //        Console.WriteLine("enter valid number");
+            //        break;
+            //}
+            //Console.WriteLine("enter two numbers");
+            //string s1 = Console.ReadLine();
+            //string s2 = Console.ReadLine();
+            //Console.WriteLine("the sum is " + (Convert.ToInt32(s1) + Convert.ToInt32(s2)));
+            //Console.ReadLine();
+
+
+            //complete calculator
+            //Console.WriteLine("Enter your choice  ");
+            //string s = Console.ReadLine();
+            //Console.WriteLine("value Entered from user is :  " + Convert.ToInt32(s));
+            //int choice = Convert.ToInt32(s);
+            //switch (choice)
+            //{
+            //    case 1:
+            //        Console.WriteLine("addition");
+            //        Console.WriteLine("Enter your choice  ");
+            //        string s1 = Console.ReadLine();
+            //        string s2 = Console.ReadLine();
+            //        Console.WriteLine("the sum is " + (Convert.ToInt32(s1) + Convert.ToInt32(s2)));
+
+
+            //        break;
+            //    case 2:
+            //        Console.WriteLine("subtraction");
+            //        Console.WriteLine("Enter two numbers ");
+            //        string s3 = Console.ReadLine();
+            //        string s4 = Console.ReadLine();
+            //        Console.WriteLine("the subtraction is " + (Convert.ToInt32(s3) - Convert.ToInt32(s4)));
+            //        break;
+            //    case 3:
+            //        Console.WriteLine("multiplication");
+            //        Console.WriteLine("Enter two numbers  ");
+            //        string s5 = Console.ReadLine();
+            //        string s6 = Console.ReadLine();
+            //        Console.WriteLine("the multiplication is " + (Convert.ToInt32(s5) * Convert.ToInt32(s6)));
+            //        break;
+            //    case 4:
+            //        Console.WriteLine("divide");
+            //        Console.WriteLine("Enter two  numbers  ");
+            //        string s7 = Console.ReadLine();
+            //        string s8 = Console.ReadLine();
+            //        Console.WriteLine("the dividation is " + (Convert.ToInt32(s7) / Convert.ToInt32(s8)));
+            //        break;
+            //    default:
+            //        Console.WriteLine("enter valid number");
+            //        break;
+            //        Console.ReadLine();
+            //        }
+
+
+            ////star pattern  for loop
+            // int i, j;
+            //Console.WriteLine("enter value of n");
+            //string s = Console.ReadLine();
+            //int n = Convert.ToInt32(s);
+            //for (i    = 1; i <= n; i++)
+            //{
+            //    for (j = 1; j <= i; j++)
+            //    {
+
+            //        Console.Write("*");
+            //    }
+
+            //    Console.WriteLine();
+            //}
+            //Console.ReadLine();
+
+
+            //pallindrome
+            //string s, revs = " ";
+            //Console.WriteLine("Enter string");
+            //s = Console.ReadLine();
+            //for (int i = s.Length - 1; i >= 0; i--)
+            //{
+            //    revs += s[i].ToString();
+            //}
+            //if(revs==s)
+            //{
+            //    Console.WriteLine("string is pallindrome");
+            //}
+            //  else
+            //{
+            //    Console.WriteLine("string is not  pallindrome");
+            //}
+             //Console.ReadLine();
+        }
+    }
+    
+}
