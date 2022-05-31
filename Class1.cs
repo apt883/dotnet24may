98a6d0d9fcd9a922dca1a2186a5ff2662530a092
avinash tangade <105920317+apt883@users.noreply.github.com>
2022-05-31 18:14:03 +0530

    Add files via upload

---
+++ Class1.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+//namespace ConsoleApp2
+ //{
+    //class Class1
+    //{
+        //   int i, j, n;
+        //   int[,] arr1 = new int[20, 20];
+        //   int[,] arr2 = new int[20, 20];
+        //   int[,] arr3 = new int[20, 20];
+        //      n=3;
+        // Console.Write("enter elements in the first  matrix :/n");
+        //  for(i=o ; i<n ; i++ ) 
+        //{
+        //     for(j=o ; i<n ; j++ )
+        //     {
+        //       arr1[i, j]=Convert.ToInt32(Console.ReadLine());
+        //     }
+        //}
+        //      Console.Write("enter elements in the  second matrix :/n");
+        //       for (i = o; i < n; i++)
+        //       { 
+        //          for (j = o; i < n; j++)
+        //          {
+        //                arr2[i, j] = Convert.ToInt32(Console.ReadLine();
+        //          }
+        //       }
+
+
+//        int[] items = { 2, 3, 5, 3, 7, 5 };
+//        int n = items.Length;
+//        Console.WriteLine ("unique array elements :");
+//            for(int i =0 ;i<n ; i++)
+//            {
+//              bool isDuplicate = false;
+//              for(int j=0 ;j<i ; j++)
+//                {
+//                  if(items[i]==items[j])
+//                 {
+//                   isDuplicate=true;
+//                  break;
+//                 }
+//}
+
+//                      if (!isDuplicate)
+//                                    Console.WriteLine(items[i]);
+//                }
+
+//            }
+//                Console.ReadLine();
+//            }
+//    }   
+//}
